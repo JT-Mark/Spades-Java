d8863488bbb64601802d6891817c1adb03009184
JTMark <33364875+TJMark@users.noreply.github.com>
2018-02-16 11:49:31 -0500

    Add files via upload

---
+++ src/spades/Spades.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spades;
+
+//imported swing library
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Joel Tucker (jo793161)
+ * Assignment 1
+ * COP 3330 Object Oriented Programming Spring 2018
+ * Whiting
+ */
+public class Spades {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        //Print out string
+        System.out.println("Welcome to Spades!");
+        //Send string to UI output using JOption Pane
+        JOptionPane.showMessageDialog(null, "Let's Play Spades!");
+        // TODO code application logic here
+        
+    } //end main 
+    
+}//end class Spades
