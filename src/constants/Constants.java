cdcc40f8effc099c75dd2627a600d6fbecb4d2ca
JTMark <33364875+TJMark@users.noreply.github.com>
2018-02-16 18:23:51 -0500

    Add files via upload

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package constants;
 
 /**
@@ -10,5 +6,28 @@ package constants;
  * @author joeltucker
  */
 public class Constants {
+    //initialize constants
+    public static final int NUM_AI = 3;
+    public static final int NUM_CARDDECK = 52;
+    public static final int NUM_CARDHAND = 13;
+    public static final int NUM_ROUNDS = 13;
+    public static final int MIN_BID = 1;
+    public static final int WIN_SCORE = 200;
     
-}
+    //add enumerations for cards
+
+    /**
+     *Colors of the cards
+     */
+    public static enum CardColor {RED, BLACK};
+    public static enum CardSuit {CLUBS, DIAMONDS, HEARTS, SPADES};
+    public static enum CardFace {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, 
+        NINE, TEN, JACK, QUEEN, KING, ACE};
+    
+    // my own addition of AI names to vary the names of the AI Players
+    public static final String[] AI_NAMES = {"Hal","Jane","Max","Wall-E","R2D2",
+        "C-3PO", "Johnny 5", "Data", "T-800", "SkyNet" }; 
+    
+    
+    
+}//end class Constants
